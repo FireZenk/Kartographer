b684e82c32886a69046882ca340300627a53592b
Jorge Garrido <firezenk@gmail.com>
2017-12-10 18:54:55 +0100

    Update README

--- sample/src/main/java/org/firezenk/kartographer/tabs/left/LeftViewRoute.kt
+++ sample/src/main/java/org/firezenk/kartographer/tabs/left/LeftViewRoute.kt
@@ -4,30 +4,14 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import org.firezenk.kartographer.annotations.RouteAnimation
-import org.firezenk.kartographer.processor.exceptions.NotEnoughParametersException
-import org.firezenk.kartographer.processor.exceptions.ParameterNotFoundException
 import org.firezenk.kartographer.processor.interfaces.Routable
 import java.util.*
-import javax.annotation.Generated
 
-@Generated("RouteProcessor")
 class LeftViewRoute<T> : Routable {
 
     override fun route(context: Any, uuid: UUID, parameters: Array<Any>, viewParent: Any?, animation: RouteAnimation?) {
-        if (parameters.isEmpty()) {
-            throw NotEnoughParametersException("Need 1 params")
-        }
-
-        if (parameters[0] !is Int) {
-            throw ParameterNotFoundException("Need int")
-        }
-
-        if (viewParent !is ViewGroup) {
-            throw ParameterNotFoundException("Need a view parent or is not a ViewGroup")
-        }
-
         val next: View = LeftView.newInstance(context as Context, uuid, parameters[0] as Int)
-        val prev: View? = viewParent.getChildAt(viewParent.childCount - 1)
+        val prev: View? = (viewParent as ViewGroup).getChildAt(viewParent.childCount - 1)
 
         animation?.let {
             prev?.let {
