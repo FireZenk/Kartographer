7b657721da367ad0f54fb0ef280e82478f92665d
Jorge Garrido <firezenk@gmail.com>
2017-12-21 17:07:26 +0100

    Route now can specify the path

--- library/src/main/java/org/firezenk/kartographer/library/types/ComplexRoute.kt
+++ library/src/main/java/org/firezenk/kartographer/library/types/ComplexRoute.kt
@@ -2,7 +2,23 @@ package org.firezenk.kartographer.library.types
 
 import java.util.*
 
-class ComplexRoute internal constructor(internal val path: Path?, internal val route: Route<*>?,
-                                        internal val viewHistory: ArrayDeque<Route<*>>) {
-    override fun toString() = route.toString() + " viewHistory size: " + viewHistory.size
+data class ComplexRoute internal constructor(internal val path: Path, internal val viewHistory: Array<Route<*>>) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ComplexRoute
+
+        if (path != other.path) return false
+        if (!Arrays.equals(viewHistory, other.viewHistory)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = path.hashCode()
+        result = 31 * result + Arrays.hashCode(viewHistory)
+        return result
+    }
 }
