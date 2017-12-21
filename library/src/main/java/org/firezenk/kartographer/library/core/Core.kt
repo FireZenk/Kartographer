6d01b0ac4bee086784fcf49209702b7670083a56
Jorge Garrido <firezenk@gmail.com>
2017-12-21 17:07:58 +0100

    Add final Core implementation

--- library/src/main/java/org/firezenk/kartographer/library/core/Core.kt
+++ library/src/main/java/org/firezenk/kartographer/library/core/Core.kt
@@ -1,7 +1,7 @@
 package org.firezenk.kartographer.library.core
 
 import org.firezenk.kartographer.library.Logger
-import org.firezenk.kartographer.library.types.ComplexRoute
+import org.firezenk.kartographer.library.dsl.route
 import org.firezenk.kartographer.library.types.Path
 import org.firezenk.kartographer.library.types.Route
 
@@ -13,29 +13,42 @@ import org.firezenk.kartographer.library.types.Route
  */
 class Core(val context: Any, var log: Logger? = null) {
 
-    val history: ArrayList<ComplexRoute> = ArrayList()
-    var lastKnownPath: Path? = null
+    companion object {
+        val ROOT_NODE: Path = Path("ROOT")
+        val DEFAULT_HISTORY: MutableMap<Route<*>, MutableList<Route<*>>> = mutableMapOf(
+                route {
+                    target = Any::class
+                    path = ROOT_NODE
+                } to mutableListOf())
+    }
+
+    var history: MutableMap<Route<*>, MutableList<Route<*>>> = DEFAULT_HISTORY
+
+    var lastKnownPath: Path = ROOT_NODE
 
     @Suppress("UNCHECKED_CAST")
     fun <B> current(): Route<B>? {
-        val route: ComplexRoute? = history.firstOrNull { it.path == lastKnownPath }
-        return route?.let {
-            if (it.viewHistory.isNotEmpty()) {
-                it.viewHistory.first as Route<B>?
-            } else {
-                it.route as Route<B>?
-            }
-        }
+        val leaf: Route<*>? = history.keys.first { it.path == lastKnownPath }
+        val branch: MutableList<Route<*>>? = history[leaf]
+        return branch?.lastOrNull() as Route<B>?
     }
 
     @Suppress("UNCHECKED_CAST")
     fun <T> payload(key: String): T? = current<Any>()?.internalParams?.get(key) as T?
 
-    fun clearHistory() = history.clear()
+    fun clearHistory() {
+        history = DEFAULT_HISTORY
+    }
 
-    fun hasHistory() = !history.isEmpty()
+    fun hasHistory() = history.isNotEmpty()
 
     fun getHistoryLast() = history.size - 1
 
-    fun getHistoryLastWithoutPath() = history.indexOfLast { it.path == null }
+    fun getHistoryLastWithoutPath() {
+        //history.indexOfLast { it.path == null }
+    }
+
+    fun pathExists(history: MutableMap<Route<*>, MutableList<Route<*>>>, route: Route<*>) = history.containsKey(route)
+
+    fun pathIsValid(route: Route<*>, prev: Route<*>?) = route.path != prev?.path
 }
