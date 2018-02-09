a523cc372ff7c129d629d5e0e9168a2cfa0433f4
Jorge Garrido <firezenk@gmail.com>
2018-02-09 11:14:35 +0100

    Solve Move class with new types

--- library/src/main/java/org/firezenk/kartographer/library/core/Move.kt
+++ library/src/main/java/org/firezenk/kartographer/library/core/Move.kt
@@ -4,8 +4,7 @@ import org.firezenk.kartographer.library.Routable
 import org.firezenk.kartographer.library.dsl.route
 import org.firezenk.kartographer.library.exceptions.NotEnoughParametersException
 import org.firezenk.kartographer.library.exceptions.ParameterNotFoundException
-import org.firezenk.kartographer.library.types.RootRoute
-import org.firezenk.kartographer.library.types.Route
+import org.firezenk.kartographer.library.types.*
 
 /**
  * Project: Kartographer
@@ -15,9 +14,14 @@ import org.firezenk.kartographer.library.types.Route
  */
 class Move(private val core: Core) {
 
-    @Suppress("UNCHECKED_CAST")
-    fun <B> routeTo(route: Route<B>) {
-        val prev: Route<B>? = core.current()
+    fun routeTo(route: Route) = when(route) {
+        is ViewRoute -> routeTo(route)
+        is ContextRoute<*> -> routeTo(route)
+        is ExternalRoute -> routeTo(route)
+    }
+
+    private fun routeTo(route: ViewRoute) {
+        val prev: Route? = core.current()
 
         try {
             core.log?.let {
@@ -27,70 +31,100 @@ class Move(private val core: Core) {
 
             core.lastKnownPath = route.path
 
-            if (route.viewParent == null) {
-                createPathRoute(route)
+            if (core.pathIsValid(route, prev) && !core.pathExists(route)) {
+                createPath(route)
+                createViewRoute(route)
+            } else if (!core.pathExists(route)) {
+                createPath(route)
             } else {
-                if (core.pathIsValid(route, prev) && !core.pathExists(route)) {
-                    createPath(route)
-                    createViewRoute(route)
-                } else if (!core.pathExists(route)) {
-                    createPath(route)
-                } else {
-                    createViewRoute(route)
-                }
+                createViewRoute(route)
             }
 
             createView(route)
         } catch (e: Throwable) {
-            when(e) {
-                is ClassCastException -> core.log?.d(" Params has to be instance of Object[] or Android's Bundle ", e)
-                is ParameterNotFoundException,
-                is NotEnoughParametersException,
-                is InstantiationException,
-                is IllegalAccessException,
-                is org.firezenk.kartographer.processor.exceptions.NotEnoughParametersException,
-                is org.firezenk.kartographer.processor.exceptions.ParameterNotFoundException -> {
-                    core.log?.d(" Navigation error: ", e.fillInStackTrace())
-                }
-                else -> throw e
-            }
+            handleError(e)
         }
+    }
+
+    private fun routeTo(route: ExternalRoute) {
+        try {
+            core.log?.let {
+                it.d(" --->> Next")
+                it.d(" Navigating to: ", route)
+            }
+
+            //TODO
+            //(route.clazz as Routable<*>)
+            //        .route(core.context, route.uuid, Any(), null, null)
+        } catch (e: Throwable) {
+            handleError(e)
+        }
+    }
+
+    private fun <B> routeTo(route: ContextRoute<B>) {
+        try {
+            core.log?.let {
+                it.d(" --->> Next")
+                it.d(" Navigating to: ", route)
+            }
+
+            core.lastKnownPath = route.path
+
+            createPathRoute(route)
+            createView(route)
+        } catch (e: Throwable) {
+            handleError(e)
+        }
+    }
+
+    private fun handleError(e: Throwable) = when(e) {
+        is ClassCastException -> core.log?.d(" Params has to be instance of Object[] or Android's Bundle ", e)
+        is ParameterNotFoundException,
+        is NotEnoughParametersException,
+        is InstantiationException,
+        is IllegalAccessException,
+        is org.firezenk.kartographer.processor.exceptions.NotEnoughParametersException,
+        is org.firezenk.kartographer.processor.exceptions.ParameterNotFoundException -> {
+            core.log?.d(" Navigation error: ", e.fillInStackTrace())
+        }
+        else -> throw e
+    }
+
+
+    private fun createView(route: ViewRoute) {
+        (route.clazz as org.firezenk.kartographer.processor.interfaces.Routable)
+                .route(core.context, route.uuid, route.params.values.toTypedArray(), route.viewParent, route.animation)
     }
 
     @Suppress("UNCHECKED_CAST")
-    private fun <B> createView(route: Route<B>) {
-        if (route.bundle != null) {
-            try {
-                (route.clazz as Routable<B>)
-                        .route(core.context, route.uuid, route.bundle as B, route.viewParent, route.animation)
-            } catch (e: ClassCastException) {
-                (route.clazz as org.firezenk.kartographer.processor.interfaces.Routable)
-                        .route(core.context, route.uuid, route.bundle!!, route.viewParent, route.animation)
-            }
-        } else {
+    private fun <B> createView(route: ContextRoute<B>) {
+        try {
+            (route.clazz as Routable<B>)
+                    .route(core.context, route.uuid, route.bundle as B, null, null)
+        } catch (e: ClassCastException) {
             (route.clazz as org.firezenk.kartographer.processor.interfaces.Routable)
-                    .route(core.context, route.uuid, (route.params as Map<String, Any>).values.toTypedArray(), route.viewParent, route.animation)
+                    .route(core.context, route.uuid, route.bundle!!, null, null)
         }
     }
 
-    private fun createPathRoute(route: Route<*>) = core.history.put(route, mutableListOf())
+    private fun createPathRoute(route: ContextRoute<*>) = core.history.put(route, mutableListOf())
 
-    private fun createViewRoute(route: Route<*>) {
-        val leaf: Route<*> = core.history.keys.first { it.path == core.lastKnownPath }
-        val branch: MutableList<Route<*>>? = core.history[leaf]
+    private fun createViewRoute(route: Route) {
+        val leaf: Route = core.history.keys.first { it.path == core.lastKnownPath }
+        val branch: MutableList<Route>? = core.history[leaf]
         branch?.add(route)
     }
 
-    private fun createPath(routeToAdd: Route<*>) {
+    private fun createPath(routeToAdd: ViewRoute) {
         val newBranch = route {
             target = RootRoute()
             path = routeToAdd.path
         }
 
-        val leaf: Route<*>? = core.history.keys.firstOrNull { it.path == routeToAdd.path }
+        val leaf: Route? = core.history.keys.firstOrNull { it?.path == routeToAdd.path }
 
         leaf?.let {
-            val branch: MutableList<Route<*>>? = core.history[leaf]
+            val branch: MutableList<Route>? = core.history[leaf]
             branch?.add(newBranch)
         } ?: core.history.put(newBranch, mutableListOf())
     }
