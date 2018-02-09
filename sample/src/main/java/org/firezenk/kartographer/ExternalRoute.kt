bb0846d1d2dde6e7b95fb2b250c37ee307535b30
Jorge Garrido <firezenk@gmail.com>
2018-02-09 16:50:26 +0100

    Add modified provided sample

---
+++ sample/src/main/java/org/firezenk/kartographer/ExternalRoute.kt
@@ -0,0 +1,38 @@
+package org.firezenk.kartographer
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import org.firezenk.kartographer.library.ExternalRoutable
+import java.util.*
+
+class ExternalRoute : ExternalRoutable {
+
+    companion object {
+        // Google play http market address
+        private val MARKET_GOOGLE_PLAY = "market://details?id="
+        // Google play http address
+        private val URL_HTTP_GOOGLE_PLAY = "https://play.google.com/store/apps/details?id="
+    }
+
+    override fun route(context: Any, uuid: UUID, parameters: Any?) {
+        if (context is Context) {
+            val packageName = context.packageName
+            try {
+                launchUri(context, Uri.parse(MARKET_GOOGLE_PLAY + packageName))
+            } catch (_: ActivityNotFoundException) {
+                launchUri(context, Uri.parse(URL_HTTP_GOOGLE_PLAY + packageName))
+            }
+        }
+    }
+
+    private fun launchUri(context: Context, uri: Uri) {
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        if (context !is Activity) {
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        context.startActivity(intent)
+    }
+}
