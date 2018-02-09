213a0f906394c78bd9c7a71a8117350c56289bba
Jorge Garrido <firezenk@gmail.com>
2018-02-09 10:51:07 +0100

    Rework dsl's for routes

--- library/src/main/java/org/firezenk/kartographer/library/dsl/Dsl.kt
+++ library/src/main/java/org/firezenk/kartographer/library/dsl/Dsl.kt
@@ -1,10 +1,14 @@
 package org.firezenk.kartographer.library.dsl
 
-import org.firezenk.kartographer.library.types.Route
+import org.firezenk.kartographer.library.types.ContextRoute
+import org.firezenk.kartographer.library.types.ExternalRoute
+import org.firezenk.kartographer.library.types.ViewRoute
 
 @DslMarker
 annotation class KartographerDsl
 
-fun route(block: RouteDsl.RouteBuilder.() -> Unit): Route<Any> = RouteDsl.RouteBuilder().apply(block).build()
+fun route(block: RouteDsl.RouteBuilder.() -> Unit): ViewRoute = RouteDsl.RouteBuilder().apply(block).build()
 
-fun <B> routeActivity(block: RouteDsl.RouteActivityBuilder<B>.() -> Unit): Route<B> = RouteDsl.RouteActivityBuilder<B>().apply(block).build()
+fun routeExternal(block: RouteDsl.RouteExternalBuilder.() -> Unit): ExternalRoute = RouteDsl.RouteExternalBuilder().apply(block).build()
+
+fun <B> routeActivity(block: RouteDsl.RouteActivityBuilder<B>.() -> Unit): ContextRoute<B> = RouteDsl.RouteActivityBuilder<B>().apply(block).build()
--- library/src/main/java/org/firezenk/kartographer/library/dsl/RouteDsl.kt
+++ library/src/main/java/org/firezenk/kartographer/library/dsl/RouteDsl.kt
@@ -2,8 +2,10 @@ package org.firezenk.kartographer.library.dsl
 
 import org.firezenk.kartographer.annotations.RouteAnimation
 import org.firezenk.kartographer.library.core.Core.Companion.ROOT_NODE
+import org.firezenk.kartographer.library.types.ContextRoute
+import org.firezenk.kartographer.library.types.ExternalRoute
 import org.firezenk.kartographer.library.types.Path
-import org.firezenk.kartographer.library.types.Route
+import org.firezenk.kartographer.library.types.ViewRoute
 import org.firezenk.kartographer.processor.interfaces.Routable
 
 @KartographerDsl
@@ -27,7 +29,7 @@ class RouteDsl {
             } else ROOT_NODE
         }
 
-        fun build(): Route<Any> = Route(target!!, params!!, path ?: calculatedPath, anchor, animation, forResult)
+        fun build(): ViewRoute = ViewRoute(target!!, params!!, path ?: calculatedPath, anchor!!, animation, forResult)
     }
 
     class RouteActivityBuilder<B> {
@@ -35,8 +37,6 @@ class RouteDsl {
         var target: Any? = null
         var params: B? = null
         var path: Path? = null
-        var anchor: Any? = null
-        var animation: RouteAnimation? = null
         var forResult: Int = -1
 
         private val calculatedPath: Path by lazy {
@@ -48,6 +48,13 @@ class RouteDsl {
             } else ROOT_NODE
         }
 
-        fun build(): Route<B> = Route<B>(target!!, params!!, path ?: calculatedPath, anchor, animation, forResult)
+        fun build(): ContextRoute<B> = ContextRoute<B>(target!!, params!!, path ?: calculatedPath, forResult)
+    }
+
+    class RouteExternalBuilder {
+
+        var target: Any? = null
+
+        fun build(): ExternalRoute = ExternalRoute(target!!)
     }
 }
